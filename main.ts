f126fbf53abd5a223446f24afda8cdee1a9ae69f
Profetonino <52037760+Profetonino@users.noreply.github.com>
2021-12-28 11:19:08 +0100

    progama en microbit coche

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "avanza") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 123)
+    }
+    if (receivedString == "atras") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 123)
+    }
+})
+radio.setGroup(1)
