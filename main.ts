e8bfce68596186f02eec3548b937039cb16976d4
Profetonino <52037760+Profetonino@users.noreply.github.com>
2021-12-28 12:18:19 +0100

    programa completo para las dos microbit

--- main.ts
+++ main.ts
@@ -1,9 +1,43 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString("para")
+    soundExpression.sad.play()
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("avanza")
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "avanza") {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 123)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 90)
     }
     if (receivedString == "atras") {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 123)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 90)
     }
+    if (receivedString == "para") {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("atras")
 })
 radio.setGroup(1)
